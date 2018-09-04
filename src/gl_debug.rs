a0e1e6208af0bb9c931cc599405ddf0e550ce44b
BradLyman <lyman.brad3211@gmail.com>
2018-09-03 19:18:09 -0700

    Fix Debug String Printing

--- src/gl_debug.rs
+++ src/gl_debug.rs
@@ -16,49 +16,57 @@ pub extern "system" fn gl_debug_to_stdout(
     message: *const GLchar,
     user: *mut std::os::raw::c_void,
 ) -> () {
+    let safe_msg = {
+        let cstr = unsafe { std::ffi::CStr::from_ptr(message) };
+
+        match cstr.to_owned().into_string() {
+            Ok(msg) => msg,
+            Err(_) => "unable to read GL debug string".to_string(),
+        }
+    };
     println!(
-        "{:?} {:?} {:?} {:?} {:?}",
+        "severity[{}] source[{}] type[{}] id[{}]: {}",
         msg_severity(severity),
         msg_source(source),
         msg_type(message_type),
         id,
-        message
+        safe_msg
     );
 }
 
 fn msg_source(source: GLenum) -> &'static str {
     match source {
-        gl::DEBUG_SOURCE_API => "source[api]",
-        gl::DEBUG_SOURCE_WINDOW_SYSTEM => "source[window system]",
-        gl::DEBUG_SOURCE_SHADER_COMPILER => "source[shader compiler]",
-        gl::DEBUG_SOURCE_THIRD_PARTY => "source[third party]",
-        gl::DEBUG_SOURCE_APPLICATION => "source[application]",
-        gl::DEBUG_SOURCE_OTHER => "source[other]",
-        _ => "source[unknown]",
+        gl::DEBUG_SOURCE_API => "api",
+        gl::DEBUG_SOURCE_WINDOW_SYSTEM => "window system",
+        gl::DEBUG_SOURCE_SHADER_COMPILER => "shader compiler",
+        gl::DEBUG_SOURCE_THIRD_PARTY => "third party",
+        gl::DEBUG_SOURCE_APPLICATION => "application",
+        gl::DEBUG_SOURCE_OTHER => "other",
+        _ => "unknown",
     }
 }
 
 fn msg_type(msg_type: GLenum) -> &'static str {
     match msg_type {
-        gl::DEBUG_TYPE_ERROR => "type[error]",
-        gl::DEBUG_TYPE_DEPRECATED_BEHAVIOR => "type[deprecated behavior]",
-        gl::DEBUG_TYPE_UNDEFINED_BEHAVIOR => "type[undefined behavior]",
-        gl::DEBUG_TYPE_PORTABILITY => "type[portability]",
-        gl::DEBUG_TYPE_PERFORMANCE => "type[performance]",
-        gl::DEBUG_TYPE_MARKER => "type[marker]",
-        gl::DEBUG_TYPE_PUSH_GROUP => "type[push group]",
-        gl::DEBUG_TYPE_POP_GROUP => "type[pop group]",
-        gl::DEBUG_TYPE_OTHER => "type[other]",
-        _ => "type[unknown]",
+        gl::DEBUG_TYPE_ERROR => "error",
+        gl::DEBUG_TYPE_DEPRECATED_BEHAVIOR => "deprecated behavior",
+        gl::DEBUG_TYPE_UNDEFINED_BEHAVIOR => "undefined behavior",
+        gl::DEBUG_TYPE_PORTABILITY => "portability",
+        gl::DEBUG_TYPE_PERFORMANCE => "performance",
+        gl::DEBUG_TYPE_MARKER => "marker",
+        gl::DEBUG_TYPE_PUSH_GROUP => "push group",
+        gl::DEBUG_TYPE_POP_GROUP => "pop group",
+        gl::DEBUG_TYPE_OTHER => "other",
+        _ => "unknown",
     }
 }
 
 fn msg_severity(severity: GLenum) -> &'static str {
     match severity {
-        gl::DEBUG_SEVERITY_HIGH => "severity[high]",
-        gl::DEBUG_SEVERITY_MEDIUM => "severity[medium]",
-        gl::DEBUG_SEVERITY_LOW => "severity[low]",
-        gl::DEBUG_SEVERITY_NOTIFICATION => "severity[notification]",
-        _ => "severity[unknown]",
+        gl::DEBUG_SEVERITY_HIGH => "high",
+        gl::DEBUG_SEVERITY_MEDIUM => "medium",
+        gl::DEBUG_SEVERITY_LOW => "low",
+        gl::DEBUG_SEVERITY_NOTIFICATION => "notification",
+        _ => "unknown",
     }
 }
