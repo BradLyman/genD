f51fd0529b242c8853f64a560e0284fe5be5aa02
BradLyman <lyman.brad3211@gmail.com>
2018-09-06 20:42:02 -0700

    Move Macros To Dedicated Module

--- src/core_gl/vao.rs
+++ src/core_gl/vao.rs
@@ -68,42 +68,6 @@ impl VAO {
     }
 }
 
-///
-/// Build a vertex attribute struct for a structure with an [f32; n] array
-/// component.
-///
-#[macro_export]
-macro_rules! vertex_attrib {
-    (@offset $s:ty, $f:ident) => {
-        unsafe {
-            let vert = std::mem::uninitialized::<$s>();
-            let addr = (&vert as *const $s) as i32;
-            let col_addr = (&vert.$f as *const _) as i32;
-            std::mem::forget(vert);
-            col_addr - addr
-        }
-    };
-    (@component_count $s:ty, $f:ident) => {
-        unsafe {
-            let vert = std::mem::uninitialized::<$s>();
-            let size = vert.$f.len();
-            std::mem::forget(vert);
-            debug_assert!(
-                size <= 4,
-                "Vertex attribute must have four or fewer components"
-            );
-            size
-        }
-    };
-    ($s:ty, $f:ident) => {
-        $crate::core_gl::vao::VertexAttrib {
-            components: vertex_attrib!(@component_count $s, $f) as i8,
-            offset: vertex_attrib!(@offset $s, $f),
-            stride: std::mem::size_of::<$s>() as i32,
-        }
-    };
-}
-
 impl Drop for VAO {
     fn drop(&mut self) {
         unsafe {
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 extern crate gl;
 extern crate sdl2;
 
+#[macro_use]
+pub mod macros;
+
 pub mod app_failure;
 pub mod core_gl;
 mod gl_app;
 mod gl_debug;
+pub mod primitives;
 
 pub use gl_app::drive_gl_app;
 pub use gl_app::GlApp;
